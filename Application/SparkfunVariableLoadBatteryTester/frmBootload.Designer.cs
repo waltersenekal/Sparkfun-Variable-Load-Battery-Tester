9fc745c7567e3720c17759790573c1c345bbcada
Walter Senekal <walter@senekal.net>
2024-02-29 15:25:31 +0200

    Added the c# application

---
+++ Application/SparkfunVariableLoadBatteryTester/frmBootload.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace SparkfunVariableLoadBatteryTester
+{
+  partial class frmBootload
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.label3 = new System.Windows.Forms.Label();
+      this.label5 = new System.Windows.Forms.Label();
+      this.textBox3 = new System.Windows.Forms.TextBox();
+      this.textBox5 = new System.Windows.Forms.TextBox();
+      this.button1 = new System.Windows.Forms.Button();
+      this.button2 = new System.Windows.Forms.Button();
+      this.progressBar1 = new System.Windows.Forms.ProgressBar();
+      this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+      this.textBox6 = new System.Windows.Forms.TextBox();
+      this.SuspendLayout();
+      // 
+      // label3
+      // 
+      this.label3.AutoSize = true;
+      this.label3.Location = new System.Drawing.Point(458, 23);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(79, 13);
+      this.label3.TabIndex = 2;
+      this.label3.Text = "USB HID State";
+      // 
+      // label5
+      // 
+      this.label5.AutoSize = true;
+      this.label5.Location = new System.Drawing.Point(25, 145);
+      this.label5.Name = "label5";
+      this.label5.Size = new System.Drawing.Size(58, 13);
+      this.label5.TabIndex = 4;
+      this.label5.Text = "Status Log";
+      // 
+      // textBox3
+      // 
+      this.textBox3.Location = new System.Drawing.Point(543, 20);
+      this.textBox3.Name = "textBox3";
+      this.textBox3.ReadOnly = true;
+      this.textBox3.Size = new System.Drawing.Size(109, 20);
+      this.textBox3.TabIndex = 7;
+      this.textBox3.Text = "Disconnected";
+      // 
+      // textBox5
+      // 
+      this.textBox5.Location = new System.Drawing.Point(28, 161);
+      this.textBox5.Multiline = true;
+      this.textBox5.Name = "textBox5";
+      this.textBox5.Size = new System.Drawing.Size(402, 152);
+      this.textBox5.TabIndex = 9;
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(451, 52);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(75, 23);
+      this.button1.TabIndex = 10;
+      this.button1.Text = "Load File";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // button2
+      // 
+      this.button2.Location = new System.Drawing.Point(545, 52);
+      this.button2.Name = "button2";
+      this.button2.Size = new System.Drawing.Size(75, 23);
+      this.button2.TabIndex = 11;
+      this.button2.Text = "Program";
+      this.button2.UseVisualStyleBackColor = true;
+      this.button2.Click += new System.EventHandler(this.button2_Click);
+      // 
+      // progressBar1
+      // 
+      this.progressBar1.Location = new System.Drawing.Point(28, 330);
+      this.progressBar1.Name = "progressBar1";
+      this.progressBar1.Size = new System.Drawing.Size(402, 23);
+      this.progressBar1.TabIndex = 12;
+      // 
+      // openFileDialog1
+      // 
+      this.openFileDialog1.FileName = "openFileDialog1";
+      // 
+      // textBox6
+      // 
+      this.textBox6.Location = new System.Drawing.Point(28, 54);
+      this.textBox6.Name = "textBox6";
+      this.textBox6.ReadOnly = true;
+      this.textBox6.Size = new System.Drawing.Size(402, 20);
+      this.textBox6.TabIndex = 13;
+      this.textBox6.Text = "C:\\dev\\walter\\Sparkfun Variable Load\\Variable_Load\\Firmware\\Variable_Load\\Variabl" +
+    "e_Load.cydsn\\CortexM3\\ARM_GCC_541\\Debug\\Variable_Load.cyacd";
+      // 
+      // frmBootLoad
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(664, 386);
+      this.Controls.Add(this.textBox6);
+      this.Controls.Add(this.progressBar1);
+      this.Controls.Add(this.button2);
+      this.Controls.Add(this.button1);
+      this.Controls.Add(this.textBox5);
+      this.Controls.Add(this.textBox3);
+      this.Controls.Add(this.label5);
+      this.Controls.Add(this.label3);
+      this.Name = "frmBootLoad";
+      this.Text = "USB Bootloader Host";
+      this.Load += new System.EventHandler(this.frmBootLoad_Load);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+
+    #endregion
+    private System.Windows.Forms.Label label3;
+    private System.Windows.Forms.Label label5;
+    private System.Windows.Forms.TextBox textBox3;
+    private System.Windows.Forms.TextBox textBox5;
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.Button button2;
+    private System.Windows.Forms.ProgressBar progressBar1;
+    private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    private System.Windows.Forms.TextBox textBox6;
+  }
+}
---
+++ Application/SparkfunVariableLoadBatteryTester/frmBootload.cs
@@ -0,0 +1,349 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;   // Marshal copy
+using System.IO;                        // For File handling
+using CyUSB;				            // For Cypress USB HID device
+
+namespace SparkfunVariableLoadBatteryTester
+{
+  public partial class frmBootload : Form
+  {
+    /// <summary>
+    /// Error values
+    /// </summary>
+    internal const int ERR_SUCCESS = 0;
+    internal const int ERR_OPEN = 1;
+    internal const int ERR_CLOSE = 2;
+    internal const int ERR_READ = 3;
+    internal const int ERR_WRITE = 4;
+
+    public CyHidDevice BootloaderHIDDevice;
+    public USBDeviceList usbHIDDevices = null;
+    int VID = 0x04B4;
+    int PID = 0xB71D;
+    String Security_Key = "000000000000";
+
+    // String holding the Cyacd file path/name
+    string Chosen_File_Cyacd = "C:\\dev\\walter\\Sparkfun Variable Load\\Variable_Load\\Firmware\\Variable_Load\\Variable_Load.cydsn\\CortexM3\\ARM_GCC_541\\Debug\\Variable_Load.cyacd";
+    bool Cyacd_found;
+
+    // For checking bootloading progress
+    double progressBarProgress;
+    double progressBarStepSize;
+    Bootloader_Utils.CyBtldr_CommunicationsData comm_data = new Bootloader_Utils.CyBtldr_CommunicationsData();
+
+    public frmBootload()
+    {
+      InitializeComponent();
+
+      usbHIDDevices = new USBDeviceList(CyConst.DEVICES_HID);
+
+      //Adding event handlers for device attachment and device removal            
+      usbHIDDevices.DeviceAttached += new EventHandler(usbHIDDevices_DeviceChecker);
+      usbHIDDevices.DeviceRemoved += new EventHandler(usbHIDDevices_DeviceChecker);
+      GetHidDevice();
+    }
+
+    /// <summary>
+    /// The GetHidDevice Method checks for the presence of a specific USB HID device and updates the status
+    /// </summary>
+    /// <returns>bool</returns>
+    public bool GetHidDevice()
+    {
+      bool Status = false;
+      try
+      {
+        // Attempt to assign local HID object to HID in devices list with matching VID and PID
+        BootloaderHIDDevice = usbHIDDevices[VID, PID] as CyHidDevice;
+
+        // If we find a valid device, update the GUI with device information
+        if (BootloaderHIDDevice != null)
+        {
+          Status = true;
+          textBox3.Text = "Connected";
+          //textBox1.Text = Convert.ToString(BootloaderHIDDevice.VendorID, 16);// BootloaderHIDDevice.VendorID.ToString();
+          //textBox1.BackColor = Color.LightBlue;
+          //textBox2.Text = Convert.ToString(BootloaderHIDDevice.ProductID, 16);//BootloaderHIDDevice.ProductID.ToString(); 
+          //textBox2.BackColor = Color.LightBlue;
+        }
+
+        // If there is no matching device, display Disconnected on GUI
+        else
+        {
+          Status = false;
+          textBox3.Text = "Disconnected";
+
+          //textBox1.BackColor = Color.LightYellow;
+          //textBox2.BackColor = Color.LightYellow;
+        }
+
+        return (Status);
+      }
+
+      catch
+      {
+        MessageBox.Show(" Device not connected");
+        return (Status);
+      }
+    }
+
+    /// <summary>
+    /// Checks if the USB device is connected and opens if it is present
+    /// Returns a success or failure
+    /// </summary>
+    public int OpenConnection()
+    {
+      int status = 0;
+      status = GetHidDevice() ? ERR_SUCCESS : ERR_OPEN;
+      return status;
+    }
+
+    /// <summary>
+    /// Closes the previously opened USB device and returns the status
+    /// </summary>
+    public int CloseConnection()
+    {
+      int status = 0;
+      BootloaderHIDDevice = null;
+      return status;
+    }
+
+    /// <summary>
+    /// Method that performs Read operation from USB Device
+    /// </summary>
+    /// <param name="buffer"> Pointer to an array where data read from USB device is copied to </param>
+    /// <param name="size"> Size of the Buffer </param>
+    /// <returns></returns>
+    public int ReadData(IntPtr buffer, int size)
+    {
+      int status;
+      bool local_status = false;
+      byte[] data = new byte[size];
+
+      //Check if USB device exists
+      if (GetHidDevice())
+      {
+        local_status = BootloaderHIDDevice.ReadInput();
+        data = BootloaderHIDDevice.Inputs.DataBuf;
+        Marshal.Copy(data, 1, buffer, Math.Min(size, data.Length));
+        status = local_status ? ERR_SUCCESS : ERR_READ;
+      }
+      else
+        status = ERR_READ;
+
+      return status;
+    }
+
+    /// <summary>
+    /// Method that writes to the USB device
+    /// </summary>
+    /// <param name="buffer">Pointer to an array where data written to USB device is stored </param>
+    /// <param name="size"> Size of the Buffer </param>
+    /// <returns></returns>
+    public int WriteData(IntPtr buffer, int size)
+    {
+      byte[] data = new byte[64];
+      bool status = true;
+
+      Marshal.Copy(buffer, data, 0, size);
+
+      if (GetHidDevice())
+      {
+        //First Byte in buffer holds the Report ID
+        BootloaderHIDDevice.Outputs.DataBuf[0] = BootloaderHIDDevice.Outputs.ID;
+
+        //Load 64 bytes of data
+        for (int i = 1; i <= size; i++)
+        {
+          BootloaderHIDDevice.Outputs.DataBuf[i] = data[i - 1];
+        }
+
+        status = BootloaderHIDDevice.WriteOutput();
+
+        if (status)
+          return ERR_SUCCESS;
+        else
+          return ERR_WRITE;
+      }
+      else
+        return ERR_WRITE;
+
+    }
+
+    /// <summary>
+    /// Method that returns the maximum transfer size
+    /// </summary>
+    public uint MaxTransferSize
+    {
+      get { return (uint)BootloaderHIDDevice.Features.RptByteLen; }
+    }
+
+    //    private void textBox1_TextChanged(object sender, EventArgs e)
+    //    {
+    //      VID = Convert.ToInt32(textBox1.Text, 16);
+    //      GetHidDevice();
+    //    }
+
+    //    private void textBox2_TextChanged(object sender, EventArgs e)
+    //    {
+    //     PID = Convert.ToInt32(textBox2.Text, 16);
+    //      GetHidDevice();
+    //}
+
+    /// <summary>
+    ///  This is the event handler for Device Connection event.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>           
+    void usbHIDDevices_DeviceChecker(object sender, EventArgs e)
+    {
+      GetHidDevice();
+    }
+
+    /// <summary>
+    /// This method handles the "Load File" Button1 Click event and lets you choose the 
+    /// appropriate CYACD file
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void button1_Click(object sender, EventArgs e)
+    {
+      int lines;
+      openFileDialog1.Title = "Open CYACD File";
+      openFileDialog1.FileName = "";
+      openFileDialog1.Filter = "cyacd file|*.cyacd";
+
+      if (openFileDialog1.ShowDialog() == DialogResult.OK)
+      {
+        Chosen_File_Cyacd = openFileDialog1.FileName;
+        textBox6.Text = Chosen_File_Cyacd;
+
+        lines = File.ReadAllLines(Chosen_File_Cyacd).Length - 1; //Don't count header
+        progressBarStepSize = 100.0 / lines;
+        Cyacd_found = true;
+      }
+    }
+
+    /// <summary>
+    /// This Method handles the "Program" Button2 Click event
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+      UpdateProgressBarInfo();
+      // The value is allowed 1 or 2. If application is using dual image, 
+      // the appID of 2nd image should be 2
+      byte appID = 1;
+
+      // Coverting string hex to byte array
+      long SKey = Int64.Parse(Security_Key, System.Globalization.NumberStyles.HexNumber);
+      byte[] SecurityKey = BitConverter.GetBytes(SKey);
+      Array.Reverse(SecurityKey);
+
+      // If Security key is not 0, make 6 bytes size array for security key 
+      // because the reversed array size is 8.
+      if (SKey == 0)
+      {
+        SecurityKey = null;
+      }
+      else
+      {
+        for (int i = 0; i < 6; i++)
+        {
+          SecurityKey[i] = SecurityKey[i + 2];
+        }
+      }
+
+      // Setup comm_data object with our delegate functions
+      comm_data.OpenConnection = OpenConnection;
+      comm_data.CloseConnection = CloseConnection;
+      comm_data.ReadData = ReadData;
+      comm_data.WriteData = WriteData;
+      comm_data.MaxTransferSize = 64;
+
+      ReturnCodes local_status = 0;
+      progressBarProgress = 0;
+      progressBar1.Value = 0;
+      // Create update object for use with Bootload_Utils.CyBtldr_Program function.  This object
+      // is used to report updated array and row information from the PSoC.
+      Bootloader_Utils.CyBtldr_ProgressUpdate update = new Bootloader_Utils.CyBtldr_ProgressUpdate(ProgressUpdate);
+
+      // Checking a HW connection and a file exist
+      if (Cyacd_found)
+      {
+        if (GetHidDevice())
+        {
+          DateTime saveNow = DateTime.Now;
+          string time_var = saveNow.ToString();
+          textBox5.Text += " Bootload Started at " + time_var + "\r\n";
+
+          local_status = (ReturnCodes)Bootloader_Utils.CyBtldr_Program(Chosen_File_Cyacd, SecurityKey, appID, ref comm_data, update);
+          saveNow = DateTime.Now;
+
+          if (local_status == (int)ReturnCodes.CYRET_SUCCESS)
+          {
+            time_var = saveNow.ToString();
+            textBox5.Text += " Bootload successful  !! \r\n Bootload ended at " + time_var + "\r\n";
+          }
+          else
+          {
+            if (ReturnCodes.CYRET_ERR_COMM_MASK == (local_status & ReturnCodes.CYRET_ERR_COMM_MASK))
+            {
+              MessageBox.Show(" Program failed: Communication Error");
+            }
+            else if (ReturnCodes.CYRET_ERR_DATA == (local_status & ReturnCodes.CYRET_ERR_DATA))
+            {
+              MessageBox.Show(" Program failed: Check Security Key");
+            }
+            else
+            {
+              MessageBox.Show(" Program failed  " + local_status.ToString());
+            }
+          }
+        }
+        else
+        {
+          textBox5.Text += "No Device Connected \r\n";
+          MessageBox.Show("No Device Connected");
+        }
+      }
+      else
+      {
+        MessageBox.Show("No file chosen");
+      }
+    }
+
+    /// <summary>
+    /// Method that updates the progres bar
+    /// </summary>
+    /// <param name="arrayID"></param>
+    /// <param name="rowNum"></param>
+    public void ProgressUpdate(byte arrayID, ushort rowNum)
+    {
+      progressBarProgress += progressBarStepSize;
+      progressBar1.Value = (int)(progressBarProgress);
+      this.Refresh();
+
+      return;
+    }
+    public void UpdateProgressBarInfo()
+    {
+      int lines = File.ReadAllLines(Chosen_File_Cyacd).Length - 1; //Don't count header
+      progressBarStepSize = 100.0 / lines;
+      Cyacd_found = true;
+    }
+
+    private void frmBootLoad_Load(object sender, EventArgs e)
+    {
+
+    }
+  }
+}
